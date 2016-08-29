2d09b19da8d3938d60567b265c02a34cb1643298
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-08-29 16:06:37 +0300

    Rearranging files

--- assets/js/Barchart.js
+++ assets/js/Barchart.js
@@ -1,28 +1,3 @@
-d3.select("#chart")
-//adding the svg
-        .append("svg")
-            .attr("width",400)
-            .attr("height",200)
-            .style("background", "#708284")
-//adding a rectangle to the svg
-        .append("rect")
-            .attr("x", 100)
-            .attr("y", 50)
-            .attr("width", 200)
-            .attr("height", 100)
-            .style("background", "#D11C24")
-//adding a circle inside the the rectangle
-d3.select("svg")
-            .append("circle")
-            .attr("cx", 200)
-            .attr("cy", 100)
-            .attr("r", 50)
-            .style("fill", "#C61C6F")
-            //.attr("opacity", 0)
-//mouse events on the circle
-.on("mouseover", function() { d3.select(this).attr("opacity", 0.3); })
-.on("mouseout", function() { d3.select(this).attr("opacity", 1); })
-
 //creating barchart
 var barData=[];
 for (var i=0; i<100; i++){
